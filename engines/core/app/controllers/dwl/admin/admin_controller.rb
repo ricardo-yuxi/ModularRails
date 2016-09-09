2098a6953395cc197e96798c3b67e49e22fcb434
Ricardo Rojas <ricardo.rojas@Pablos-iMac.local>
2016-09-09 12:01:23 -0500

    creates admin_controller, route && link

---
+++ engines/core/app/controllers/dwl/admin/admin_controller.rb
@@ -0,0 +1,8 @@
+module Dwl
+  module Admin
+    class AdminController < ApplicationController
+      def index
+      end
+    end
+  end
+end
--- engines/core/config/routes.rb
+++ engines/core/config/routes.rb
@@ -1,4 +1,7 @@
 Dwl::Core::Engine.routes.draw do
   devise_for :users, class_name: "Dwl::User", module: :devise
+  namespace :admin do
+    get '/' => 'admin#index'
+  end
   root to: "dashboard#index"
 end
